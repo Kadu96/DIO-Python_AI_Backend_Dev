d35dbaed3c265ed7d95cd4f1c11d921be81403a2
Carlos Eduardo <cadusilva96@gmail.com>
2024-06-12 11:04:59 -0300

    .

---
+++ Primeiros_Passos_com_Python/03_conversao_tipos.py
@@ -0,0 +1,14 @@
+preco_int = 10
+print(preco_int)
+
+preco_float = float(preco_int)
+print(preco_float)
+
+print(preco_int / 2) # / retorna um valor float
+print(preco_int // 2)# // retorna um valor inteiro
+
+preco = "10.50"
+idade = '28'
+
+print(float(preco) * 2)
+print(int(idade))
