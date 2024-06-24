ca1cd5020d1258d87b3509347b1f7747edb92c92
Carlos Eduardo <cadusilva96@gmail.com>
2024-06-24 16:00:36 -0300

    modified:   POO/16_manipulacao_arquivos_csv.py

--- POO/16_manipulacao_arquivos_csv.py
+++ POO/16_manipulacao_arquivos_csv.py
@@ -6,7 +6,8 @@ COL_ID = 0
 COL_NOME = 1
 
 try:
-    with open(ROOT_PATH/"arquivos_teste"/"usuarios.csv", "w", newline="", encoding="utf-8") as arquivo:
+    with open(ROOT_PATH/"arquivos_teste"/"usuarios.csv", "w", newline="", 
+              encoding="utf-8") as arquivo:
         escritor = csv.writer(arquivo)
         escritor.writerow(['id', 'nome'])
         escritor.writerow(['1', 'carlos'])
@@ -15,7 +16,8 @@ except IOError as exc:
     print(f"Erro na abertura do arqivo!\n{exc}")
     
 try:
-    with open(ROOT_PATH/"arquivos_teste"/"usuarios.csv", "r", newline="", encoding="utf-8") as arquivo:
+    with open(ROOT_PATH/"arquivos_teste"/"usuarios.csv", "r", newline="", 
+              encoding="utf-8") as arquivo:
         leitor = csv.reader(arquivo)
         for row in leitor:
             print(row[COL_ID], row[COL_NOME])
@@ -24,10 +26,11 @@ except IOError as exc:
 print()
     
 try:
-    with open(ROOT_PATH/"arquivos_teste"/"usuarios.csv", "r", newline="", encoding="utf-8") as arquivo:
+    with open(ROOT_PATH/"arquivos_teste"/"usuarios.csv", "r", newline="", 
+              encoding="utf-8") as arquivo:
         leitor = csv.DictReader(arquivo)
         for row in leitor:
             print(f"ID: {row['id']}")
             print(f"Nome: {row['nome']}")
 except IOError as exc:
-    print(f"Erro na abertura do arqivo!\n{exc}")    
+    print(f"Erro na abertura do arqivo!\n{exc}")
